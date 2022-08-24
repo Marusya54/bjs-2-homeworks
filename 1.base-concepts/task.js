a874e6e2d942c112d5a9a402b2b896fd15a46941
Marusya54 <marina_koneva@bk.ru>
2022-08-24 13:57:53 +0300

    first commit variables

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,22 @@
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = [];
+  let d = b ** 2 - 4 * a * c; 
+  if (d > 0) {
+    arr[0] = (-b + Math.sqrt(d)) / (2 * a);
+    arr[1] = (-b - Math.sqrt(d)) / (2 * a);
+    
+  } else if (d === 0) {
+    arr[0] = -b / (2 * a);
+    
+  } else if (d < 0) {
+    arr;
+  }
+
   return arr; // array
 }
+
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
