64d55b58cbb8dff833afeadfbd0eba89662230a2
Marusya54 <marina_koneva@bk.ru>
2022-09-14 12:44:14 +0300

    first commit variables

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,8 +1,73 @@
+//Задача №1
+
 function cachingDecoratorNew(func) {
-  // Ваш код
-}
+  let cache = [];
+  
+  function wrapper(...args) {
+    const hash = args.join(',');
+    let objectInCache = cache.findIndex((item) => item.hash === hash);
+    if (objectInCache !== -1) {
+      console.log("Из кэша: " + cache[objectInCache].result);
+      return "Из кэша: " + cache[objectInCache].result;
+    }; 
 
+    let result = func(...args);
+    cache.push({ hash, result });
+    if (cache.length > 5) {
+      cache.shift();
+    };
 
-function debounceDecoratorNew(func) {
-  // Ваш код
-}
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+};
+
+return wrapper;
+};
+
+const addThree = (a, b, c) => a + b + c;
+const upgradedAddThree = cachingDecoratorNew(addThree);
+upgradedAddThree(1, 2, 3); // вычисляем: 6
+upgradedAddThree(1, 2, 3); // из кэша: 6
+upgradedAddThree(2, 2, 3); // вычисляем: 7
+upgradedAddThree(3, 2, 3); // вычисляем: 8
+upgradedAddThree(4, 2, 3); // вычисляем: 9
+upgradedAddThree(5, 2, 3); // вычисляем: 10
+upgradedAddThree(6, 2, 3); // вычисляем: 11   (при этом кэш для 1, 2, 3 уничтожается)
+upgradedAddThree(1, 2, 3); // вычисляем: 6  (снова вычисляем, кэша нет)
+
+//Задача №2
+
+function debounceDecoratorNew(func, ms) {
+  let timerId = null;
+
+  function wrapper(...args){
+    if (timerId === null) {
+      func(...args);
+  };
+
+    clearTimeout(timerId);
+    
+      timerId = setTimeout(() => {
+        return func(...args)
+      }, ms)
+
+      timerId = setTimeout(() => func(...args), ms)
+    };
+
+    return wrapper;
+};
+
+const sendSignal = (signalOrder, delay) => console.log("Сигнал отправлен", signalOrder, delay);
+const upgradedSendSignal = debounceDecoratorNew(sendSignal, 2000);
+setTimeout(() => upgradedSendSignal(1, 0)); // Сигнал отправлен + будет запланирован асинхронный запуск, который будет проигнорирован так как следующий сигнал отменит предыдущий (300 - 0 < 2000)
+setTimeout(() => upgradedSendSignal(2, 300), 300); // проигнорировано так как следующий сигнал отменит предыдущий (900 - 300 < 2000)
+setTimeout(() => upgradedSendSignal(3, 900), 900); // проигнорировано так как следующий сигнал отменит предыдущий (1200 - 900 < 2000)
+setTimeout(() => upgradedSendSignal(4, 1200), 1200); // проигнорировано так как следующий сигнал отменит предыдущий (2300 - 1200 < 2000)
+setTimeout(() => upgradedSendSignal(5, 2300), 2300); // Сигнал отправлен так как следующий вызов не успеет отменить текущий: 4400-2300=2100 (2100 > 2000)
+setTimeout(() => upgradedSendSignal(6, 4400), 4400); // проигнорировано так как следующий сигнал отменит предыдущий (4500 - 4400 < 2000)
+setTimeout(() => upgradedSendSignal(7, 4500), 4500); // Сигнал будет отправлен, так как последний вызов debounce декоратора (спустя 4500 + 2000 = 6500) 6,5с
+setTimeout(() => {
+  console.log(upgradedSendSignal.count); // было выполнено 3 отправки сигнала
+  console.log(upgradedSendSignal.allCount); // было выполнено 6 вызовов декорированной функции
+}, 7000)
+  
